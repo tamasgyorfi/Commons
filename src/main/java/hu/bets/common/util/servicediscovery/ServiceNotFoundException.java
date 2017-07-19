2f5a41335a64bd0fc0882b0ad3aa283eda1730cc
Tamas Gyorfi <gyorfitamas@gmail.com>
2017-07-19 23:49:57 +0100

    service discovery factored out

--- src/main/java/hu/bets/common/util/servicediscovery/ServiceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package hu.bets.common.util.servicediscovery;
-
-public class ServiceNotFoundException extends RuntimeException {
-    public ServiceNotFoundException(Exception e) {
-        super(e);
-    }
-}
--- src/main/java/hu/bets/common/util/servicediscovery/UnableToRegisterServiceException.java
+++
@@ -1,8 +0,0 @@
-package hu.bets.common.util.servicediscovery;
-
-public class UnableToRegisterServiceException extends RuntimeException {
-
-    public UnableToRegisterServiceException(String msg, Exception e) {
-        super(msg, e);
-    }
-}
